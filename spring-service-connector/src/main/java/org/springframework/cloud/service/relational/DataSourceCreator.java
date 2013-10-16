0ade97ee41744dff707cb7c47509e54eefd28a44
Ramnivas Laddad <rladdad@gopivotal.com>
2013-10-16 14:43:44 -0700

    WIP

--- spring-service-connector/src/main/java/org/springframework/cloud/service/relational/DataSourceCreator.java
+++ spring-service-connector/src/main/java/org/springframework/cloud/service/relational/DataSourceCreator.java
@@ -1,18 +1,20 @@
 package org.springframework.cloud.service.relational;
 
+import static org.springframework.cloud.service.Util.hasClass;
+
 import java.sql.DriverManager;
+import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.cloud.Cloud;
 import org.springframework.cloud.service.AbstractServiceConnectorCreator;
 import org.springframework.cloud.service.ServiceConnectorConfig;
 import org.springframework.cloud.service.ServiceConnectorCreationException;
 import org.springframework.cloud.service.common.RelationalServiceInfo;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-
-import static org.springframework.cloud.service.Util.*;
 
 /**
  * 
@@ -22,6 +24,8 @@ import static org.springframework.cloud.service.Util.*;
  */
 public abstract class DataSourceCreator<SI extends RelationalServiceInfo> extends AbstractServiceConnectorCreator<DataSource, SI> {
 
+	private static Logger logger = Logger.getLogger(DataSourceCreator.class.getName());
+	
 	private DataSourceConfigurer configurer = new DataSourceConfigurer();
 	
 	public abstract String getDriverClassName();
@@ -34,16 +38,19 @@ public abstract class DataSourceCreator<SI extends RelationalServiceInfo> extend
 			Class.forName(getDriverClassName());
 			// Give first preference to user's DBCP datasource
 			if (hasClass("org.apache.commons.dbcp.BasicDataSource")) {
+				logger.info("Found DBCP on the classpath. Using it for DataSource connection pooling.");
 				org.apache.commons.dbcp.BasicDataSource ds = new org.apache.commons.dbcp.BasicDataSource();
 				setBasicDataSourceProperties(ds, serviceInfo, serviceConnectorConfig);
 				return ds;
 				// else, we have one from Tomcat
-			} else if (hasClass("org.apache.tomcat.dbcp.dbcp.BasicDataSource")) {
-				org.apache.tomcat.dbcp.dbcp.BasicDataSource ds = new org.apache.tomcat.dbcp.dbcp.BasicDataSource();
+			} else if (hasClass("org.apache.tomcat.jdbc.pool.DataSource")) {
+				logger.info("Found Tomcat connection pool on the classpath. Using it for DataSource connection pooling.");
+				org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource();
 				setBasicDataSourceProperties(ds, serviceInfo, serviceConnectorConfig);
 				return ds;
 			} else {
 				// Only for testing outside Tomcat/CloudFoundry
+				logger.warning("Found neither DBCP nor Tomcat connection pool on the classpath (no pooling in effect)");
 				return new SimpleDriverDataSource(DriverManager.getDriver(serviceInfo.getJdbcUrl()),
 						serviceInfo.getJdbcUrl());
 			}
