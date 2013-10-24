8303f61e8888c66d7f5a2e7ac7bc8fdb6b27f7ab
Ramnivas Laddad <rladdad@gopivotal.com>
2013-10-24 12:28:34 -0700

    Add tests to verify that label-based rabbit payload works fine

--- cloudfoundry-connector/src/test/java/org/springframework/cloud/cloudfoundry/CloudFoundryConnectorRabbitServiceTest.java
+++ cloudfoundry-connector/src/test/java/org/springframework/cloud/cloudfoundry/CloudFoundryConnectorRabbitServiceTest.java
@@ -15,13 +15,13 @@ import org.springframework.cloud.service.ServiceInfo;
  */
 public class CloudFoundryConnectorRabbitServiceTest extends AbstractCloudFactoryConnectorTest {
 	@Test
-	public void rabbitServiceCreation() {
+	public void rabbitServiceCreationWithTags() {
 		String[] versions = {"2.0", "2.2"};
 		for (String version : versions) {
 			when(mockEnvironment.getEnvValue("VCAP_SERVICES"))
 				.thenReturn(getServicesPayload(
-						getRabbitServicePayload(version, "rabbit-1", hostname, port, username, password, "q-1", "vhost1"),
-						getRabbitServicePayload(version, "rabbit-2", hostname, port, username, password, "q-2", "vhost2")));
+				        getRabbitServicePayloadWithTags(version, "rabbit-1", hostname, port, username, password, "q-1", "vhost1"),
+				        getRabbitServicePayloadWithTags(version, "rabbit-2", hostname, port, username, password, "q-2", "vhost2")));
 		}
 
 		List<ServiceInfo> serviceInfos = testCloudConnector.getServiceInfos();
@@ -29,22 +29,52 @@ public class CloudFoundryConnectorRabbitServiceTest extends AbstractCloudFactory
 		assertNotNull(getServiceInfo(serviceInfos, "rabbit-2"));
 	}
 
-	private String getRabbitServicePayload(String version, String serviceName,
+    @Test
+    public void rabbitServiceCreationWithoutTags() {
+        String[] versions = {"1.0", "1.1"};
+        for (String version : versions) {
+            when(mockEnvironment.getEnvValue("VCAP_SERVICES"))
+                .thenReturn(getServicesPayload(
+                        getRabbitServicePayloadWithoutTags(version, "rabbit-1", hostname, port, username, password, "q-1", "vhost1"),
+                        getRabbitServicePayloadWithoutTags(version, "rabbit-2", hostname, port, username, password, "q-2", "vhost2")));
+        }
+
+        List<ServiceInfo> serviceInfos = testCloudConnector.getServiceInfos();
+        assertNotNull(getServiceInfo(serviceInfos, "rabbit-1"));
+        assertNotNull(getServiceInfo(serviceInfos, "rabbit-2"));
+    }
+
+    private String getRabbitServicePayloadWithoutTags(String version, String serviceName,
+            String hostname, int port,
+            String user, String password, String name,
+            String vHost) {
+        return getRabbitServicePayload("test-rabbit-info-with-label-no-tags.json", version, serviceName, 
+                                       hostname, port, user, password, name, vHost);
+    }
+	
+	private String getRabbitServicePayloadWithTags(String version, String serviceName,
                                 			  String hostname, int port,
                                 			  String user, String password, String name,
                                 			  String vHost) {
-		String payload = readTestDataFile("test-rabbit-info.json");
-		payload = payload.replace("$version", version);
-		payload = payload.replace("$serviceName", serviceName);
-		payload = payload.replace("$hostname", hostname);
-		payload = payload.replace("$port", Integer.toString(port));
-		payload = payload.replace("$user", user);
-		payload = payload.replace("$pass", password);
-		payload = payload.replace("$name", name);
-		payload = payload.replace("$virtualHost", vHost);
-		
-		return payload;
+		return getRabbitServicePayload("test-rabbit-info.json", version, serviceName, 
+		                               hostname, port, user, password, name, vHost);
 	}
 
+    private String getRabbitServicePayload(String filename, String version, String serviceName,
+                                           String hostname, int port,
+                                           String user, String password, String name,
+                                           String vHost) {
+        String payload = readTestDataFile(filename);
+        payload = payload.replace("$version", version);
+        payload = payload.replace("$serviceName", serviceName);
+        payload = payload.replace("$hostname", hostname);
+        payload = payload.replace("$port", Integer.toString(port));
+        payload = payload.replace("$user", user);
+        payload = payload.replace("$pass", password);
+        payload = payload.replace("$name", name);
+        payload = payload.replace("$virtualHost", vHost);
+        
+        return payload;
+    }
 	
 }
