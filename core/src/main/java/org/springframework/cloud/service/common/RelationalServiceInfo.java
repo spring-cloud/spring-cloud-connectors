e55c1d1230b8c7eb42ae457553d64577923a2125
Scott Frederick <scottyfred@gmail.com>
2014-05-13 18:08:14 -0500

    Changed JDBC URL creation to retain query parameters from URI in service credentials.

--- core/src/main/java/org/springframework/cloud/service/common/RelationalServiceInfo.java
+++ core/src/main/java/org/springframework/cloud/service/common/RelationalServiceInfo.java
@@ -3,29 +3,48 @@ package org.springframework.cloud.service.common;
 import org.springframework.cloud.service.UriBasedServiceInfo;
 
 /**
- * 
  * @author Ramnivas Laddad
- *
  */
 public abstract class RelationalServiceInfo extends UriBasedServiceInfo {
 
-    protected String jdbcUrlDatabaseType;
+	protected String jdbcUrlDatabaseType;
 
-    public RelationalServiceInfo(String id, String uriString, String jdbcUrlDatabaseType) {
-        super(id, uriString);
-        this.jdbcUrlDatabaseType = jdbcUrlDatabaseType;
-    }
+	public RelationalServiceInfo(String id, String uriString, String jdbcUrlDatabaseType) {
+		super(id, uriString);
+		this.jdbcUrlDatabaseType = jdbcUrlDatabaseType;
+	}
 
-    @ServiceProperty(category="connection")
-    public String getJdbcUrl() {
-        if (getPort() != -1) {
-            return  String.format("jdbc:%s://%s:%d/%s?user=%s&password=%s",
-                                   jdbcUrlDatabaseType, getHost(), getPort(), getPath(), 
-                                   getUserName(), getPassword());
-        } else {
-            return  String.format("jdbc:%s://%s/%s?user=%s&password=%s",
-                                  jdbcUrlDatabaseType, getHost(), getPath(), 
-                                  getUserName(), getPassword());
-        }
-    }
+	@ServiceProperty(category = "connection")
+	public String getJdbcUrl() {
+		return String.format("jdbc:%s://%s%s/%s%s%s", jdbcUrlDatabaseType, getHost(), formatPort(),
+				getPath(), formatUserinfo(), formatQuery());
+	}
+
+	private String formatPort() {
+		if (getPort() != -1) {
+			return String.format(":%d", getPort());
+		}
+		return "";
+	}
+
+	private String formatUserinfo() {
+		if (getUserName() != null && getPassword() != null) {
+			return String.format("?user=%s&password=%s", getUserName(), getPassword());
+		}
+		if (getUserName() != null) {
+			return String.format("?user=%s", getUserName());
+		}
+		return "";
+	}
+
+	private String formatQuery() {
+		if (getQuery() != null) {
+			if (getUserName() == null && getPassword() == null) {
+				return String.format("?%s", getQuery());
+			} else {
+				return String.format("&%s", getQuery());
+			}
+		}
+		return "";
+	}
 }
---
+++ core/src/test/java/org/springframework/cloud/service/common/RelationalServiceInfoTest.java
@@ -0,0 +1,54 @@
+package org.springframework.cloud.service.common;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RelationalServiceInfoTest {
+
+	@Test
+	public void jdbcFullUrl() {
+		RelationalServiceInfo serviceInfo = createServiceInfo("dbtype://username:password@hostname:1234/database");
+
+		assertEquals("jdbc:jdbcdbtype://hostname:1234/database?user=username&password=password", serviceInfo.getJdbcUrl());
+	}
+
+	@Test
+	public void jdbcUrlNoPort() {
+		RelationalServiceInfo serviceInfo = createServiceInfo("dbtype://username:password@hostname/database");
+
+		assertEquals("jdbc:jdbcdbtype://hostname/database?user=username&password=password", serviceInfo.getJdbcUrl());
+	}
+
+	@Test
+	public void jdbcUrlNoUsernamePassword() {
+		RelationalServiceInfo serviceInfo = createServiceInfo("dbtype://hostname:1234/database");
+
+		assertEquals("jdbc:jdbcdbtype://hostname:1234/database", serviceInfo.getJdbcUrl());
+	}
+
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void jdbcUrlNoPassword() {
+		createServiceInfo("dbtype://username@hostname/database");
+	}
+
+	@Test
+	public void jdbcUrlWithQuery() {
+		RelationalServiceInfo serviceInfo = createServiceInfo("dbtype://username:password@hostname:1234/database?reconnect=true");
+
+		assertEquals("jdbc:jdbcdbtype://hostname:1234/database?user=username&password=password&reconnect=true", serviceInfo.getJdbcUrl());
+	}
+
+	@Test
+	public void jdbcUrlWithQueryNoUsernamePassword() {
+		RelationalServiceInfo serviceInfo = createServiceInfo("dbtype://hostname:1234/database?reconnect=true");
+
+		assertEquals("jdbc:jdbcdbtype://hostname:1234/database?reconnect=true", serviceInfo.getJdbcUrl());
+	}
+
+	private RelationalServiceInfo createServiceInfo(final String uri) {
+		return new RelationalServiceInfo("test", uri, "jdbcdbtype") {
+		};
+	}
+
+}
