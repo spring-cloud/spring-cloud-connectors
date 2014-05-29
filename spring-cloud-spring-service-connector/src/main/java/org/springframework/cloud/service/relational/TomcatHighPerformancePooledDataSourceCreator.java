22ba1441591eb8bb7afc2b4026e83efcd2235dc0
Ramnivas Laddad <rladdad@gopivotal.com>
2014-05-29 08:21:49 -0700

    Differentiate logs between regular and high-performance tomcat pool

--- spring-cloud-spring-service-connector/src/main/java/org/springframework/cloud/service/relational/TomcatHighPerformancePooledDataSourceCreator.java
+++ spring-cloud-spring-service-connector/src/main/java/org/springframework/cloud/service/relational/TomcatHighPerformancePooledDataSourceCreator.java
@@ -20,7 +20,7 @@ public class TomcatHighPerformancePooledDataSourceCreator<SI extends RelationalS
 	public DataSource create(RelationalServiceInfo serviceInfo, ServiceConnectorConfig serviceConnectorConfig,
                              String driverClassName, String validationQuery) {
 		if (hasClass("org.apache.tomcat.jdbc.pool.DataSource")) {
-			logger.info("Found Tomcat connection pool on the classpath. Using it for DataSource connection pooling.");
+			logger.info("Found Tomcat high-performance connection pool on the classpath. Using it for DataSource connection pooling.");
 			org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource();
 			setBasicDataSourceProperties(ds, serviceInfo, serviceConnectorConfig, driverClassName, validationQuery);
 			return ds;
