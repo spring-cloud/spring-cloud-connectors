0abf58f4918f75129539ae9185f67483bf838c9a
Christopher Smith <chrylis@gmail.com>
2014-07-09 17:33:53 -0500

    remove unused import

--- spring-cloud-spring-service-connector/src/main/java/org/springframework/cloud/config/ServiceScanHelper.java
+++ spring-cloud-spring-service-connector/src/main/java/org/springframework/cloud/config/ServiceScanHelper.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudException;
 import org.springframework.cloud.CloudFactory;
 import org.springframework.cloud.config.java.ServiceScan;
 import org.springframework.cloud.service.GenericCloudServiceConnectorFactory;
