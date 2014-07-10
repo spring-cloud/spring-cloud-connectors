0629e84768c73e007bd5d5349fc306ffff0a2d5f
Christopher Smith <chrylis@gmail.com>
2014-07-09 19:29:07 -0500

    pull up MySQL URI scheme

--- spring-cloud-core/src/main/java/org/springframework/cloud/service/common/MysqlServiceInfo.java
+++ spring-cloud-core/src/main/java/org/springframework/cloud/service/common/MysqlServiceInfo.java
@@ -3,14 +3,16 @@ package org.springframework.cloud.service.common;
 import org.springframework.cloud.service.ServiceInfo.ServiceLabel;
 
 /**
- * 
+ *
  * @author Ramnivas Laddad
  *
  */
 @ServiceLabel("mysql")
 public class MysqlServiceInfo extends RelationalServiceInfo {
 
+    public static final String URI_SCHEME = "mysql";
+
 	public MysqlServiceInfo(String id, String url) {
-		super(id, url, "mysql");
+		super(id, url, URI_SCHEME);
 	}
 }
