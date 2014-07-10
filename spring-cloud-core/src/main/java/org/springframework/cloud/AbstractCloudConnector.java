839a72569c81e82c696b97d65977db18b9dd1899
Christopher Smith <chrylis@gmail.com>
2014-07-09 19:06:07 -0500

    pull up KeyValuePair class from HerokuConnector to AbstractCloudConnector

--- spring-cloud-core/src/main/java/org/springframework/cloud/AbstractCloudConnector.java
+++ spring-cloud-core/src/main/java/org/springframework/cloud/AbstractCloudConnector.java
@@ -10,25 +10,25 @@ import org.springframework.cloud.service.ServiceInfo;
 
 /**
  * Helper abstract class to simplify {@link CloudConnector} implementations.
- * 
+ *
  * User the {@link ServiceLoader} approach to looks for file name matching the class passed in constructor
  * and registers {@link ServiceInfoCreator} found there.
- * 
+ *
  * Implementation of {@link CloudConnector}s that wish to support the recommended service scanning approach
  * should extends this approach to gain that functionality automatically.
- * 
+ *
  * @author Ramnivas Laddad
  *
  */
 public abstract class AbstractCloudConnector<SD> implements CloudConnector {
 
 	private static Logger logger = Logger.getLogger(AbstractCloudConnector.class.getName());
-	
+
 	protected List<ServiceInfoCreator<?,SD>> serviceInfoCreators = new ArrayList<ServiceInfoCreator<?,SD>>();
 
 	protected abstract List<SD> getServicesData();
 	protected abstract FallbackServiceInfoCreator<?,SD> getFallbackServiceInfoCreator();
-	
+
 	public AbstractCloudConnector(Class<? extends ServiceInfoCreator<? extends ServiceInfo, ?>> serviceInfoCreatorClass) {
 		scanServiceInfoCreators(serviceInfoCreatorClass);
 	}
@@ -39,7 +39,7 @@ public abstract class AbstractCloudConnector<SD> implements CloudConnector {
 		for (SD serviceData : getServicesData()) {
 			serviceInfos.add(getServiceInfo(serviceData));
 		}
-		
+
 		return serviceInfos;
 	}
 
@@ -54,18 +54,36 @@ public abstract class AbstractCloudConnector<SD> implements CloudConnector {
 			registerServiceInfoCreator(serviceInfoCreator);
 		}
 	}
-	
+
 	private ServiceInfo getServiceInfo(SD serviceData) {
 		for (ServiceInfoCreator<? extends ServiceInfo,SD> serviceInfoCreator : serviceInfoCreators) {
 			if (serviceInfoCreator.accept(serviceData)) {
 				return serviceInfoCreator.createServiceInfo(serviceData);
 			}
 		}
-		
+
 		// Fallback with a warning
 		ServiceInfo fallackServiceInfo = getFallbackServiceInfoCreator().createServiceInfo(serviceData);
 		logger.warning("No suitable service info creator found for service " + fallackServiceInfo.getId()
 				+ " Did you forget to add a ServiceInfoCreator?");
 		return fallackServiceInfo;
 	}
+
+    public static class KeyValuePair {
+    	private final String key;
+    	private final String value;
+
+    	public KeyValuePair(String key, String value) {
+    		this.key = key;
+    		this.value = value;
+    	}
+
+    	public String getKey() {
+    		return key;
+    	}
+
+    	public String getValue() {
+    		return value;
+    	}
+    }
 }
--- spring-cloud-heroku-connector/src/main/java/org/springframework/cloud/heroku/HerokuServiceInfoCreator.java
+++ spring-cloud-heroku-connector/src/main/java/org/springframework/cloud/heroku/HerokuServiceInfoCreator.java
@@ -1,11 +1,11 @@
 package org.springframework.cloud.heroku;
 
+import org.springframework.cloud.AbstractCloudConnector.KeyValuePair;
 import org.springframework.cloud.ServiceInfoCreator;
-import org.springframework.cloud.heroku.HerokuConnector.KeyValuePair;
 import org.springframework.cloud.service.ServiceInfo;
 
 /**
- * 
+ *
  * @author Ramnivas Laddad
  *
  */
@@ -16,25 +16,25 @@ public abstract class HerokuServiceInfoCreator<SI extends ServiceInfo> implement
 	public HerokuServiceInfoCreator(String urlProtocol) {
 		this.urlProtocol = urlProtocol;
 	}
-	
+
 	public boolean accept(KeyValuePair serviceData) {
 		return serviceData.getValue().toString().startsWith(urlProtocol + "://");
 	}
-	
+
 	public abstract SI createServiceInfo(String id, String uri);
-	
+
 	public SI createServiceInfo(KeyValuePair serviceData) {
 		return createServiceInfo(serviceData.getKey(), serviceData.getValue());
 	}
-	
+
 	/**
 	 * Get prefixes for env variable with which the associated {@link ServiceInfo} may be created.
-	 * 
+	 *
 	 * Unlike CloudFoundry which exposes VCAP_SERVICES as a single environment to encompass all services bound
 	 * to the app, Heroku expose one environment variable per app. This method allows each info creator to declare
-	 * appropriate env variables. 
-	 * 
-	 * @return prefixes for the relevant environment variables 
+	 * appropriate env variables.
+	 *
+	 * @return prefixes for the relevant environment variables
 	 */
 	public abstract String[] getEnvPrefixes();
 }
