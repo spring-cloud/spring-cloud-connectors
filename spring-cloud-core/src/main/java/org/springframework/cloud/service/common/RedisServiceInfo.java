b46aa7addfa2bfdfdcc03e9476aab6a17a8325bc
Christopher Smith <chrylis@gmail.com>
2014-07-09 19:38:52 -0500

    pull up Redis URI scheme

--- spring-cloud-core/src/main/java/org/springframework/cloud/service/common/RedisServiceInfo.java
+++ spring-cloud-core/src/main/java/org/springframework/cloud/service/common/RedisServiceInfo.java
@@ -4,16 +4,19 @@ import org.springframework.cloud.service.UriBasedServiceInfo;
 import org.springframework.cloud.service.ServiceInfo.ServiceLabel;
 
 /**
- * 
+ *
  * @author Ramnivas Laddad
  *
  */
 @ServiceLabel("redis")
 public class RedisServiceInfo extends UriBasedServiceInfo {
+
+    public static final String URI_SCHEME = "redis";
+
 	public RedisServiceInfo(String id, String host, int port, String password) {
-		super(id, "redis", host, port, null, password, null);
+		super(id, URI_SCHEME, host, port, null, password, null);
 	}
-	
+
 	public RedisServiceInfo(String id, String uri) {
         super(id, uri);
     }
