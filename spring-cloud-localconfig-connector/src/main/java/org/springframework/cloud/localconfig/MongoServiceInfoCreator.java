7d894c2c082b8422cb67b1f788218094073362fe
Christopher Smith <chrylis@gmail.com>
2014-07-10 17:01:14 -0500

    add ServiceInfoCreators and SPI control files

---
+++ spring-cloud-localconfig-connector/src/main/java/org/springframework/cloud/localconfig/MongoServiceInfoCreator.java
@@ -0,0 +1,20 @@
+package org.springframework.cloud.localconfig;
+
+import org.springframework.cloud.service.common.MongoServiceInfo;
+
+/**
+ *
+ * @author Christopher Smith
+ *
+ */
+public class MongoServiceInfoCreator extends LocalConfigServiceInfoCreator<MongoServiceInfo>{
+
+    public MongoServiceInfoCreator() {
+        super(MongoServiceInfo.URI_SCHEME);
+    }
+
+    @Override
+    public MongoServiceInfo createServiceInfo(String id, String uri) {
+        return new MongoServiceInfo(id, uri);
+    }
+}
