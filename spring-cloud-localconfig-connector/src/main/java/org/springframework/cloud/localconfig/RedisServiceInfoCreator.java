8afa0cdcbd7d3e30ad320b37ef8e419632bd8c49
Christopher Smith <chrylis@gmail.com>
2014-07-10 17:01:14 -0500

    add ServiceInfoCreators and SPI control files

---
+++ spring-cloud-localconfig-connector/src/main/java/org/springframework/cloud/localconfig/RedisServiceInfoCreator.java
@@ -0,0 +1,20 @@
+package org.springframework.cloud.localconfig;
+
+import org.springframework.cloud.service.common.RedisServiceInfo;
+
+/**
+ *
+ * @author Christopher Smith
+ *
+ */
+public class RedisServiceInfoCreator extends LocalConfigServiceInfoCreator<RedisServiceInfo>{
+
+    public RedisServiceInfoCreator() {
+        super(RedisServiceInfo.URI_SCHEME);
+    }
+
+    @Override
+    public RedisServiceInfo createServiceInfo(String id, String uri) {
+        return new RedisServiceInfo(id, uri);
+    }
+}
