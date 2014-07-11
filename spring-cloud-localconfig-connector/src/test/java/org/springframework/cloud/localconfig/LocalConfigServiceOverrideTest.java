c42a8e3ba7ffe9cb043bc4e869cb90a2ab17d5f4
Christopher Smith <chrylis@gmail.com>
2014-07-11 16:41:18 -0500

    Add test to override programmatic definition from system properties.
    fixes #45

---
+++ spring-cloud-localconfig-connector/src/test/java/org/springframework/cloud/localconfig/LocalConfigServiceOverrideTest.java
@@ -0,0 +1,34 @@
+package org.springframework.cloud.localconfig;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ProvideSystemProperty;
+import org.springframework.cloud.service.ServiceInfo;
+import org.springframework.cloud.service.common.MongoServiceInfo;
+
+public class LocalConfigServiceOverrideTest extends AbstractLocalConfigConnectorTest {
+
+    @Rule
+    public final ProvideSystemProperty OVERRIDE_MYSQL =
+        new ProvideSystemProperty(
+            "spring.cloud.candygram",
+            "mongodb://youruser:yourpass@40.30.20.10:4321/dbname");
+
+    @Test
+    public void serviceOverride() {
+        List<ServiceInfo> services = connector.getServiceInfos();
+        ServiceInfo service = getServiceInfo(services, "candygram");
+        assertNotNull(service);
+        assertTrue(service instanceof MongoServiceInfo);
+        MongoServiceInfo mongo = (MongoServiceInfo) service;
+        assertEquals("youruser", mongo.getUserName());
+        assertEquals(4321, mongo.getPort());
+    }
+
+}
