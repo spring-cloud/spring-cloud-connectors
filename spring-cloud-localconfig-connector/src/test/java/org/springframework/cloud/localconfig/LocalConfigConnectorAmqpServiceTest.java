14272c8b104eb757d50a6a7fa0b3bfa71bf6334a
Christopher Smith <chrylis@gmail.com>
2014-07-11 16:28:09 -0500

    tests for all ServiceInfoCreators

---
+++ spring-cloud-localconfig-connector/src/test/java/org/springframework/cloud/localconfig/LocalConfigConnectorAmqpServiceTest.java
@@ -0,0 +1,23 @@
+package org.springframework.cloud.localconfig;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.cloud.service.ServiceInfo;
+import org.springframework.cloud.service.common.AmqpServiceInfo;
+
+public class LocalConfigConnectorAmqpServiceTest extends AbstractLocalConfigConnectorTest {
+
+    @Test
+    public void serviceCreation() {
+        List<ServiceInfo> services = connector.getServiceInfos();
+        ServiceInfo service = getServiceInfo(services, "rabbit");
+        assertNotNull(service);
+        assertTrue(service instanceof AmqpServiceInfo);
+        assertUriParameters((AmqpServiceInfo) service);
+    }
+
+}
