646a99fb34668b08becd56d7ede40de5d598cfc6
Christopher Smith <chrylis@gmail.com>
2014-07-11 16:28:09 -0500

    tests for all ServiceInfoCreators
    fixes #45

---
+++ spring-cloud-localconfig-connector/src/test/java/org/springframework/cloud/localconfig/LocalConfigConnectorRedisServiceTest.java
@@ -0,0 +1,23 @@
+package org.springframework.cloud.localconfig;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.cloud.service.ServiceInfo;
+import org.springframework.cloud.service.common.RedisServiceInfo;
+
+public class LocalConfigConnectorRedisServiceTest extends AbstractLocalConfigConnectorTest {
+
+    @Test
+    public void serviceCreation() {
+        List<ServiceInfo> services = connector.getServiceInfos();
+        ServiceInfo service = getServiceInfo(services, "blue");
+        assertNotNull(service);
+        assertTrue(service instanceof RedisServiceInfo);
+        assertUriParameters((RedisServiceInfo) service);
+    }
+
+}
