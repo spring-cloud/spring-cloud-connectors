d777e4da17fc4b9a6625de5ba98a167640038069
Christopher Smith <chrylis@gmail.com>
2014-07-16 19:53:09 -0500

    add getSystemProperties() method

--- spring-cloud-core/src/main/java/org/springframework/cloud/util/EnvironmentAccessor.java
+++ spring-cloud-core/src/main/java/org/springframework/cloud/util/EnvironmentAccessor.java
@@ -3,28 +3,33 @@ package org.springframework.cloud.util;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Map;
+import java.util.Properties;
 
 import org.springframework.cloud.CloudConnector;
 import org.springframework.cloud.CloudException;
 
 /**
  * Environment available to the deployed app.
- * 
+ *
  * The main purpose of this class is to allow unit-testing of {@link CloudConnector} implementations
  * that rely on environment
- * 
+ *
  * @author Ramnivas Laddad
  */
 public class EnvironmentAccessor {
-	
+
 	public Map<String, String> getEnv() {
 		return System.getenv();
 	}
-	
+
 	public String getEnvValue(String key) {
 		return System.getenv(key);
 	}
-	
+
+	public Properties getSystemProperties() {
+	    return System.getProperties();
+	}
+
 	public String getSystemProperty(String key) {
 		return System.getProperty(key);
 	}
