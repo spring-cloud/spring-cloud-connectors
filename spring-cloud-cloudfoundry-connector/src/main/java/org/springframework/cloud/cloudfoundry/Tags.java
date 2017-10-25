5896a0b8b15f1206b19420cf43c36290369a416c
Scott Frederick <sfrederick@pivotal.io>
2017-10-25 16:01:24 -0500

    Make all tags comparisons case-insensitive in Cloud Foundry connector.

--- spring-cloud-cloudfoundry-connector/src/main/java/org/springframework/cloud/cloudfoundry/Tags.java
+++ spring-cloud-cloudfoundry-connector/src/main/java/org/springframework/cloud/cloudfoundry/Tags.java
@@ -1,6 +1,5 @@
 package org.springframework.cloud.cloudfoundry;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Tags {
@@ -17,8 +16,10 @@ public class Tags {
 	public boolean containsOne(List<String> tags) {
 		if (tags != null) {
 			for (String value : values) {
-				if (tags.contains(value)) {
-					return true;
+				for (String tag : tags) {
+					if (tag.equalsIgnoreCase(value)) {
+						return true;
+					}
 				}
 			}
 		}
@@ -26,17 +27,28 @@ public class Tags {
 	}
 
 	public boolean contains(String tag) {
-		return tag != null && Arrays.asList(values).contains(tag);
-	}
-
-	public boolean startsWith(String tag) {
 		if (tag != null) {
 			for (String value : values) {
-				if (tag.startsWith(value)) {
+				if (tag.equalsIgnoreCase(value)) {
 					return true;
 				}
 			}
 		}
 		return false;
+	}
+
+	public boolean startsWith(String tag) {
+		if (tag != null) {
+			for (String value : values) {
+				if (startsWithIgnoreCase(tag, value)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean startsWithIgnoreCase(String string, String prefix) {
+		return string.regionMatches(true, 0, prefix, 0, prefix.length());
 	}
 }
